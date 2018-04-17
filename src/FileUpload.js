990a06d2959336cd33658458b53d0408e343d045
Miguel Ángel Navas Giraldo <pronick@gmail.com>
2018-04-17 23:06:52 +0200

    Branding white & green

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 
-
 class FileUpload extends Component {
-    constructor() {
-        // siempre inicializar el padre si extiende
-        super();
-        // declaramos el objeto de estado de react
-        this.state = {
-            uploadValue: 0
-        };
-    }
+  // constructor() {
+  //   // siempre inicializar el padre si extiende
+  //   super();
+  // }
 
-
-    render() {
-        return (
-            <div>
-                {/* barra de progreso */}
-                <progress value={this.state.uploadValue} max='100'></progress>
-                <br />
-                <input type="file" onChange={this.props.onUpload} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {/* barra de progreso */}
+        <progress value={this.props.uploadValue} max="100" />
+        <br />
+        <input type="file" onChange={this.props.onUpload} />
+      </div>
+    );
+  }
 }
 
 export default FileUpload;
